da7efcc7c5e3b5fad8addd9ab46741cc67029ae1
Jacquiiii <jacqui.koroll@telus.com>
2022-11-06 19:22:38 -0800

    kata 2

--- kata1.js
+++ kata1.js
@@ -6,7 +6,7 @@ const sumLargestNumbers = function(arr) {
 
 for(let i = 0; i < arr.length; i++) {
   if(arr[i] > largest) {
-    secondLargest = largest; //largest is tracked as 2nd largest so value doesn't get lost and can be used in else if
+    secondLargest = largest; //largest is tracked as 2nd largest so value doesn't get lost and can be used in else
     largest = arr[i]; // largest takes value of highest number
   } else if (arr[i] >= secondLargest && arr[i] != largest) {
     secondLargest = arr[i]; 
---
+++ kata2.js
@@ -0,0 +1,26 @@
+const conditionalSum = function(values, condition) {
+  if (values.length === 0) {
+    return 0;
+  } else if (condition === "odd") {
+    let oddTotal = 0;
+    for (let i = 0; i < values.length; i++) {
+      if (values[i] % 2 !== 0) {
+        oddTotal += values[i];
+      }
+    }
+    return oddTotal;
+  } else if (condition === "even") {
+    let evenTotal = 0;
+    for (let i = 0; i < values.length; i++) {
+      if (values[i] % 2 === 0) {
+        evenTotal += values[i];
+      }
+    }
+    return evenTotal;
+  }
+}
+
+console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
+console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
+console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
+console.log(conditionalSum([], "odd"));
