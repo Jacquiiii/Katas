ce34e0c91a7761b1c90da3938c2ef4f4c9de778c
Jacquiiii <jacqui.koroll@telus.com>
2022-11-08 11:48:05 -0800

    Kata 8 first solution

--- Kata-8.js
+++ Kata-8.js
@@ -1,7 +1,19 @@
 const repeatNumbers = data => {
 
+  let str = "";
 
+  for (let x of data) {
+    str += ", ";
+    for (let i = 0; i < x[1]; i++) {
+      str += x[0];
+    }
+  }
+
+  return str.slice(2);
 }
 
 
 console.log(repeatNumbers([[1, 10]]));
+console.log(repeatNumbers([[1, 2], [2, 3]]));
+console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]]));
+console.log(repeatNumbers([[10, 4], [34, 6], [92, 2], [1, 10], [1, 10]]));
