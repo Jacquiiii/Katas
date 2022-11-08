9811d4e186078948bf1ce15128de97d15e24c00a
Jacquiiii <jacqui.koroll@telus.com>
2022-11-08 14:46:30 -0800

    updating katas

--- Kata-8.js
+++ Kata-8.js
@@ -17,3 +17,34 @@ console.log(repeatNumbers([[1, 10]]));
 console.log(repeatNumbers([[1, 2], [2, 3]]));
 console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]]));
 console.log(repeatNumbers([[10, 4], [34, 6], [92, 2], [1, 10], [1, 10]]));
+
+
+
+
+/* 
+
+Alternate solution:
+
+const repeatNumbers = data => {
+  let temp = ""; let result =""; //Placeholders to store loop and result data
+  init = 0; // Initilize - prevents ", " from being added on first loop.
+
+  for (let x of data){ //Nested loop to check each input array
+    for (let i = 0; i < x[1]; i++){ //Loop length is equal to index 1 of each array
+      temp += x[0] //repeat storing index 0 value at each loop
+    }
+
+   if (init > 0){
+    result += ", " + temp //Adds each array together seperated by ,
+   } else {
+    result += temp //adds temp if first loop of array
+   }
+    
+   temp = ""; //Clears temp before entering next for of loop.
+   init++ // increase init so ", " is added to all following loops
+  };
+  
+  return result // Return combined result
+};
+
+*/
--- Kata-9.js
+++ Kata-9.js
@@ -1,6 +1,43 @@
 const camelCase = input => {
+  
+  let lowercaseWords = input.toLowerCase();
+  let arr = lowercaseWords.split(' ');
+  arr[0].toLowerCase();
+
+  for (let i = 1; i < arr.length; i++) {
+      arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1); 
+    }
+
+  return arr.join("");
+
+  }
 
 
-}
+console.log(camelCase("Hello jacqui koroll"));
+console.log(camelCase("this is a string"));
+console.log(camelCase("loopy lighthouse"));
+console.log(camelCase("supercalifragalisticexpialidocious"));
 
-console.log(camelCase("Hello Jacqui"));
+
+/* 
+
+Alternative:
+
+const camelCase = input => {
+  newString = ""; 
+  input = input.toLowerCase(); 
+
+  for (let i = 0; i < input.length; i++){
+    if (input[i] === " " && input[i + 1] === " "){
+      // do nothing here, remains empty
+    } 
+    else if (input[i] === " "){
+      newString += input[i + 1].toUpperCase(); 
+      i++;
+    } 
+    else {newString += input[i];} 
+  }
+
+  return newString; 
+
+*/
