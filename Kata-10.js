e01a10a73650119f4b051b7a09f4b7fbca3e87ff
Jacquiiii <jacqui.koroll@telus.com>
2022-11-08 18:04:50 -0800

    adding updated Kata 10 with alternative

--- Kata-10.js
+++ Kata-10.js
@@ -8,9 +8,28 @@ const multiplicationTable = function (maxValue) {
     str += "\n";
   }
 
-  return str;
+  return str.slice(0, -1);
 }
 
 console.log(multiplicationTable(1));
 console.log(multiplicationTable(5));
 console.log(multiplicationTable(10));
+
+
+/*
+Alternative with blank line after
+
+const multiplicationTable = function (maxValue) {
+  let str = "";
+  
+  for (let x = 1; x <= maxValue; x++) {
+    for (let i = 1; i <= maxValue; i++) {
+      str += (x * i) + " ";
+    }
+    str += "\n";
+  }
+
+  return str;
+}
+
+*/
