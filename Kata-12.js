7f51a5d729f46d7f7548cf014287b468548483df
Jacquiiii <jacqui.koroll@telus.com>
2022-11-09 20:23:06 -0800

    adding finished Kata 12

--- Kata-12.js
+++ Kata-12.js
@@ -1,20 +1,73 @@
+// helper function that produces array of ingredients for a specific bakery
 const ingredientCheck = (bakery, recipes) => {
-
+  let matchingIngredients = [];
   for (let x of bakery) {
     for (let i = 0; i < recipes.length; i++) {
       for (let j = 0; j < recipes[i].ingredients.length; j++) {
         if (x === recipes[i].ingredients[j]) {
-          console.log(recipes[i].ingredients[j]);
-        } 
+          matchingIngredients.push(x);
+        }
       }
     }
   }
-
+  return matchingIngredients;
 }
 
-let bakeryA = ['potatoes', 'bay leaf', 'raisins'];
-let bakeryB = ['red bean', 'dijon mustard', 'apples'];
+
+const chooseRecipe = (bakeryA, bakeryB, recipes) => {
+  
+  let recipeName = "";
+
+  let ingredientsA = ingredientCheck(bakeryA, recipes); // produces array of ingredients from bakeryA that match recipe
+
+  let ingredientsB = ingredientCheck(bakeryB, recipes); // produces array of ingredients from bakeryB that match recipe
+
+  let allIngredients = ingredientsA.concat(ingredientsB); // combines ingredients from bakeryA & bakeryB that match recipe
+
+  for (recipe of recipes) {
+    for (let i = 0; i < recipe.ingredients.length; i++) {
+
+      let firstIngredient = recipe.ingredients[i] //saves first ingredient in recipe provided upcoming if condition is met
+
+      let secondIngredient = recipe.ingredients[i+1] //saves second ingredient in recipe provided upcoming if condition is met (if more ingredients, would need to revamp function)
+
+      if (allIngredients.includes(firstIngredient) && allIngredients.includes(secondIngredient)) {
+        recipeName = recipe.name //checks if ingredients are included in the combined ingredient variable made prior to for loop and pushes the chosen recipe name to the recipe name variable if true
+      }
+
+    }
+    
+  };
+
+  return recipeName;
+}
+
+
+
+// test conditions:
+
+let bakeryA = ['saffron', 'eggs', 'tomato paste', 'coconut', 'custard'];
+let bakeryB = ['milk', 'butter', 'cream cheese'];
 let recipes = [
+    {
+        name: 'Coconut Sponge Cake',
+        ingredients: ['coconut', 'cake base']
+    },
+    {
+        name: 'Persian Cheesecake',
+        ingredients: ['saffron', 'cream cheese']
+    },
+    {
+        name: 'Custard Surprise',
+        ingredients: ['custard', 'ground beef']
+    }
+];
+
+console.log(chooseRecipe(bakeryA, bakeryB, recipes));
+
+bakeryA = ['potatoes', 'bay leaf', 'raisins'];
+bakeryB = ['red bean', 'dijon mustard', 'apples'];
+recipes = [
     {
         name: 'Potato Ganache',
         ingredients: ['potatoes', 'chocolate']
@@ -26,42 +79,6 @@ let recipes = [
     {
         name: "Nima's Famous Dijon Raisins",
         ingredients: ['dijon mustard', 'raisins']
-    }
-];
-
-const chooseRecipe = (bakeryA, bakeryB, recipes) => {
-  
-  let recipeName = "";
-  let ingredientsA = ingredientCheck(bakeryA, recipes);
-  let ingredientsB = ingredientCheck(bakeryB, recipes);
-
-  for (let i = 0; i < recipes.length; i++) {
-    for (let j = 0; j < recipes[i].ingredients.length; j++) {
-      for
-    }
-  }
-  
-  return recipeName;
-}
-
-
-
-// test conditions:
-
-bakeryA = ['potatoes', 'bay leaf', 'raisins'];
-bakeryB = ['red bean', 'dijon mustard', 'apples'];
-recipes = [
-    {
-      name: 'Potato Ganache',
-      ingredients: ['potatoes', 'chocolate']
-    },
-    {
-      name: 'Sweet Fish',
-      ingredients: ['anchovies', 'honey']
-    },
-    {
-      name: "Nima's Famous Dijon Raisins",
-      ingredients: ['dijon mustard', 'raisins']
     }
 ];
 
