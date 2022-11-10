1376c95bc8f8c0c5fad44518b9a4d62fcfc8ef81
Jacquiiii <jacqui.koroll@telus.com>
2022-11-10 14:07:57 -0800

    testing

---
+++ Kata-15.js
@@ -0,0 +1,20 @@
+const organizeInstructors = instructors => {
+  
+  let result = {};
+
+  for (let i = 0; i < instructors.length; i++) {
+    for (let x = 0; x < instructors.course.length; x++) {
+      
+    }
+  }
+
+};
+
+
+
+console.log(organizeInstructors([
+  {name: "Samuel", course: "iOS"},
+  {name: "Victoria", course: "Web"},
+  {name: "Karim", course: "Web"},
+  {name: "Donald", course: "Web"}
+]));
