d28508eb19dc99095e8a3f6081aefcdd5cff7458
Jacquiiii <jacqui.koroll@telus.com>
2022-11-10 17:31:19 -0800

    adding kata 15

--- Kata-15.js
+++ Kata-15.js
@@ -1,13 +1,18 @@
 const organizeInstructors = instructors => {
   
-  let result = {};
+  let result = {}; //empty object where for loop will add the keys and properties
 
-  for (let i = 0; i < instructors.length; i++) {
-    for (let x = 0; x < instructors.course.length; x++) {
-      
-    }
-  }
+    for (let i = 0; i < instructors.length; i++) {
 
+      //if statement prevents the same course being added multiple times
+      if (result.hasOwnProperty(instructors[i].course) === false) {
+        result[instructors[i].course] = []; //creates empty array for each course
+      }
+
+      result[instructors[i].course].push(instructors[i].name); //pushes the name to the empty array from the if statement above (for all iterations not limited like the if statement)
+    };
+
+  return result;
 };
 
 
@@ -17,4 +22,10 @@ console.log(organizeInstructors([
   {name: "Victoria", course: "Web"},
   {name: "Karim", course: "Web"},
   {name: "Donald", course: "Web"}
+]));
+console.log(organizeInstructors([
+  {name: "Brendan", course: "Blockchain"},
+  {name: "David", course: "Web"},
+  {name: "Martha", course: "iOS"},
+  {name: "Carlos", course: "Web"}
 ]));
